fad9456aeee1d4e921d2b8f44af7d62e9bdfbf07
gkaguirre <gkaguirre@me.com>
2018-03-02 17:19:08 -1000

    Quiver and area plots
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/eyeposeQuantificationError.m
+++ code/eyeposeQuantificationError.m
@@ -59,29 +59,47 @@ end
 %% Reshape the eye poses into lists suitable for quivering
 veridicalAziList = reshape(eyePoses(:,:,1),nAzi*nEle,1);
 veridicalEleList = reshape(eyePoses(:,:,2),nAzi*nEle,1);
-inverseAziList1SDerror = reshape(inverseEyePose1SDerror(:,:,1),nAzi*nEle,1);
-inverseEleList1SDerror = reshape(inverseEyePose1SDerror(:,:,2),nAzi*nEle,1);
-inverseAziList2SDerror = reshape(inverseEyePose2SDerror(:,:,1),nAzi*nEle,1);
-inverseEleList2SDerror = reshape(inverseEyePose2SDerror(:,:,2),nAzi*nEle,1);
+inverseAziList1SDerror = reshape(inverseEyePose1SDerror(:,:,1),nAzi*nEle,1)-veridicalAziList;
+inverseEleList1SDerror = reshape(inverseEyePose1SDerror(:,:,2),nAzi*nEle,1)-veridicalEleList;
+inverseAziList2SDerror = reshape(inverseEyePose2SDerror(:,:,1),nAzi*nEle,1)-veridicalAziList;
+inverseEleList2SDerror = reshape(inverseEyePose2SDerror(:,:,2),nAzi*nEle,1)-veridicalEleList;
 
 figure
 
 %% Quiver
-subplot(2,1,1)
-quiver(veridicalAziList,veridicalEleList,inverseAziList2SDerror,inverseEleList2SDerror,'-r');
+subplot(1,2,1)
+h1=quiver(veridicalAziList,veridicalEleList,inverseAziList2SDerror,inverseEleList2SDerror,0,'LineWidth',2,'ShowArrowHead','off');
+h1.Color=[1 0.5 0.5];
 hold on
-q=quiver(veridicalAziList,veridicalEleList,inverseAziList1SDerror,inverseEleList1SDerror);
-q.Color = [1 0.4 0.6];
+h2=quiver(veridicalAziList,veridicalEleList,inverseAziList1SDerror,inverseEleList1SDerror,0,'LineWidth',2,'ShowArrowHead','off');
+h2.Color=[.75 0.75 0.75];
+hold off
+scale=.9;
+hU1 = get(h1,'UData');
+hV1 = get(h1,'VData');
+set(h1,'UData',scale*hU1,'VData',scale*hV1)
+hU2 = get(h2,'UData');
+hV2 = get(h2,'VData');
+set(h2,'UData',scale*hU2,'VData',scale*hV2)
+hold on
 plot(veridicalAziList,veridicalEleList,'.k');
-axis equal
+xlim([-5 30]);
+ylim([-5 30]);
+axis square
+titleString=sprintf('Max error 1D, 2D Azi = %4.2f, %4.2f; Ele = %4.2f, %4.2f',max(inverseAziList1SDerror),max(inverseAziList2SDerror),max(inverseEleList1SDerror),max(inverseEleList2SDerror));
+title(titleString);
 
 %% Circle
 veridicalRadii = ones(nAzi*nEle,1)*pupilRadiusMM;
-inverseRadii1SDerror = reshape(inverseEyePose1SDerror(:,:,4),nAzi*nEle,1);
-inverseRadii2SDerror = reshape(inverseEyePose2SDerror(:,:,4),nAzi*nEle,1);
-subplot(2,1,2)
-viscircles([veridicalAziList veridicalEleList],inverseRadii2SDerror,'Color',[1 0 0],'LineWidth',0.5);
+inverseRadii1SDerror = reshape(inverseEyePose1SDerror(:,:,4),nAzi*nEle,1)-veridicalRadii;
+inverseRadii2SDerror = reshape(inverseEyePose2SDerror(:,:,4),nAzi*nEle,1)-veridicalRadii;
+subplot(1,2,2)
+viscircles([veridicalAziList veridicalEleList],inverseRadii2SDerror*10,'Color',[1 0.5 0.5],'LineWidth',0.5);
 hold on
-viscircles([veridicalAziList veridicalEleList],inverseRadii1SDerror,'Color',[1 0.5 0.5],'LineWidth',0.5);
-viscircles([veridicalAziList veridicalEleList],veridicalRadii,'Color',[.5 .5 .5],'LineWidth',0.5);
-axis equal;
+viscircles([veridicalAziList veridicalEleList],inverseRadii1SDerror*10,'Color',[.75 0.75 0.75],'LineWidth',0.5);
+plot(veridicalAziList,veridicalEleList,'.k');
+xlim([-5 30]);
+ylim([-5 30]);
+axis square;
+titleString=sprintf('Error 1D, 2D Min = %4.2f, %4.2f; Max = %4.2f, %4.2f',min(inverseRadii1SDerror),min(inverseRadii2SDerror),max(inverseRadii1SDerror),max(inverseRadii2SDerror));
+title(titleString);
