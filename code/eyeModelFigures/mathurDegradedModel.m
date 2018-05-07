7d930c13fb00fc525dfae6940a1638ecedb405cc
gkaguirre <gkaguirre@me.com>
2018-05-06 21:38:35 -0400

    Added Fedtke fit
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/eyeModelFigures/mathurDegradedModel.m
+++ code/eyeModelFigures/mathurDegradedModel.m
@@ -58,7 +58,7 @@ for modelLevel = 1:nModels
             sg.eye.pupil.eccenFcnString = '@(x) 0';
         case 4
             sg = createSceneGeometry('sphericalAmetropia',sphericalAmetropia,'spectralDomain','vis');
-        case 4
+        case 5
             sg = createSceneGeometry('sphericalAmetropia',sphericalAmetropia,'spectralDomain','vis');
             sg.refraction.opticalSystem(1,5) = 1.25;
     end
@@ -66,6 +66,11 @@ for modelLevel = 1:nModels
         [diamRatios(modelLevel,vv), C(modelLevel,vv), pupilFitError(modelLevel,vv), thetas(modelLevel,vv), horizPixels(modelLevel,vv), vertPixels(modelLevel,vv) ] = returnPupilDiameterRatio_CameraMoves(viewingAngleDeg(vv),exitPupilDiam,sg);
     end
 end
+
+
+% Mathur 2013 Equation 7. Used to fit the pupil diameter values and extract
+% the turning point (beta).
+mathurEq7 = fittype( @(beta,D,E,x) D.*cosd((x-beta)./E), 'independent','x','dependent','y');
 
 % This is Eq 9 from Mathur 2013, which specifies the horizontal to vertical
 % ratio of the entrance pupil from different viewing angles relative to
@@ -75,6 +80,13 @@ mathurEq9 = @(viewingAngleDeg) 0.99.*cosd((viewingAngleDeg+5.3)/1.121);
 % This is Eq 11, which specifies the oblique component of pupil ellipticity
 mathurEq11 = @(viewingAngleDeg) 0.00072.*viewingAngleDeg-0.0008;
 
+% Fedtke 2013 model Figure 9, 6mm pupil, viewing angle x pupil diam ratio
+fedtkeFig9Data = [   78.0328   70.3279   57.7049   51.6393   44.4262   35.2459   28.0328   11.9672    1.8033; ...
+    0.4057    0.5009    0.6447    0.7082    0.7779    0.8541    0.9048    0.9789    0.9979];
+
+fedtkeFit = fit (fedtkeFig9Data(1,:)',fedtkeFig9Data(2,:)',eq7,'StartPoint',[5.3,0.93,1.12]);
+
+        
 % Plot the results.
 figure
 
@@ -85,7 +97,7 @@ for modelLevel = 1:nModels
     plot(viewingAngleDeg,mathurEq9(viewingAngleDeg) ,'-','Color',[.5 .5 .5]);
     plot(viewingAngleDeg,cosd(viewingAngleDeg),':','Color',[.5 .5 .5]);
     plot(viewingAngleDeg,diamRatios(modelLevel,:),'-','Color',[1 0 0]);
-%    plot(viewingAngleDeg,horizPixels(modelLevel,:)./vertPixels(modelLevel,:),'-','Color',[0 1 0]);
+    plot(viewingAngleDeg,fedtkeFit(viewingAngleDeg),'-g');
     RMSE = sqrt(mean((diamRatios(modelLevel,:)-mathurEq9(viewingAngleDeg)).^2));
     text(0,0.6,sprintf('RMSE = %1.0e',RMSE),'HorizontalAlignment','Center');
 
